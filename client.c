8ed6e61cfefc74257726eb23e1001c1d5698b0d0
mcdonn2 <mcdonn2@device.finalproject-group4.ch-geni-net.instageni.colorado.edu>
2020-04-27 14:48:49 -0600

    :q

--- client.c
+++ client.c
@@ -13,61 +13,110 @@
 #include <sys/socket.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <time.h>
+#include <sys/select.h>
 
 #include <arpa/inet.h>
 
 #define PORT "3490" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define BILLION 1000000000.0
+
+void * get_in_addr(struct sockaddr *);
+
+double RTT[4];
+
+typedef struct subnet_t {
+	int towerNum;
+	char *ip;
+}subnet;
+
 
 void * clientThread(void *server)
 {
 	printf("In thread\n");
-	char message[1000];
-	char buffer[1024];
-	int clientSocket;
-	struct sockaddr_in serverAddr;
-	socklen_t addr_size;
-
-	int sockfd, numbytes;
+	int sockfd, numbytes;  
+        char buf[MAXDATASIZE];
         struct addrinfo hints, *servinfo, *p;
         int rv;
         char s[INET6_ADDRSTRLEN];
+	struct timespec start, end;
 
-	//Create the socket
-	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
 
-	//Configure settings of the server address
-	//Address family is internet
-	serverAddr.sin_family = AF_INET;
+	printf("address: %s\n", ((subnet *)server)->ip);
+        if ((rv = getaddrinfo(((subnet *)server)->ip, PORT, &hints, &servinfo)) != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+                exit(1);
+        }
 
-	//set port number, using htons function
-	serverAddr.sin_port = htons(7799);
+        // loop through all the results and connect to the first we can
+        for(p = servinfo; p != NULL; p = p->ai_next) {
+                if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                                p->ai_protocol)) == -1) {
+                        perror("client: socket");
+                        continue;
+                }
 
-	//Set IP address to localhost
-	serverAddr.sin_addr.s_addr = inet_addr((char *)server);
-	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+                if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+                        perror("client: connect");
+                        close(sockfd);
+                        continue;
+                }
 
-	//Connect the socket to the server using the address
-	addr_size = sizeof(serverAddr);
-	connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-	strcpy(message, "Hello");
+                break;
+        }
 
-	if( send(clientSocket, message, strlen(message), 0)  < 0)
+        if (p == NULL) {
+                fprintf(stderr, "client: failed to connect\n");
+                exit(1);
+        }
+
+        inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+                        s, sizeof s);
+        printf("client: connecting to %s\n", s);
+
+        freeaddrinfo(servinfo); // all done with this structure
+
+        if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+            perror("recv");
+            exit(1);
+        }
+
+        buf[numbytes] = '\0';
+
+        printf("client: received '%s'\n",buf);
+
+	char * data = calloc(1024, sizeof(char));
+
+	while(1)
 	{
-		printf("Send failed\n");
+		//ping loop
+		
+		clock_gettime(CLOCK_REALTIME, &start);
+		if (send(sockfd, data, 1024, 0) == -1)
+			perror("send");
+
+
+		if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1)
+		{
+			perror("recv");
+		}
+		clock_gettime(CLOCK_REALTIME, &end);
+		buf[numbytes] = '\0';
+        	
+		double time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
+		RTT[((subnet *)server)->towerNum] = time_spent;
+		//printf("client: ping '%s' from %s took %f ms\n\n",buf, s, time_spent * 1000);
+		
+		sleep(5);
 	}
 
-	//read the message from the server into the buffer
-	if(recv(clientSocket, buffer, 1024, 0) < 0)
-	{
-		printf("Receive failed\n");
-	}
-	//print the received message
-	printf("Data received: %s\n", buffer);
-	close(clientSocket);
+        close(sockfd);
 	pthread_exit(NULL);
-
 
 }
 
@@ -81,84 +130,49 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
 	pthread_t tid[5];
 	int i = 0;
-	char addresses[4][8] = {"10.0.4.1", "10.0.3.1", "10.0.2.1", "10.0.1.1"};
+	//char addresses[4][9] = {"10.0.1.1\0", "10.0.2.1\0", "10.0.3.1\0", "10.0.4.1\0"};
+	subnet nets[4] = {
+	{.towerNum = 0, .ip = "10.0.1.1\0"},
+	{.towerNum = 1, .ip = "10.0.2.1\0"},
+	{.towerNum = 2, .ip = "10.0.3.1\0"},
+	{.towerNum = 3, .ip = "10.0.4.1\0"}};
 
 	while(i < 4)
 	{
-		if(pthread_create(&tid[i], NULL, clientThread, (void *)&addresses[i]) != 0)
+		if(pthread_create(&tid[i], NULL, clientThread, (void *)&nets[i]) != 0)
 			printf("Failed to create thread\n");
 		i++;
+		sleep(1);
 	}
-	sleep(20);
+	
+	//int time = 30;
+	while(!(getc(stdin) == 'x'))
+	{
+		printf("\nCurrent ping values:\n0: %f ms\n1: %f ms\n2: %f ms\n3: %f ms\n\n", 
+				1000 * RTT[0], 1000 * RTT[1], 1000 * RTT[2], 1000 * RTT[3]);
+
+		int bestTower = 0;
+		for (i = 1; i < 4; i++)
+		{
+			if (RTT[i] < RTT[bestTower])
+				bestTower = i;
+		}
+		printf("The most optimal route is tower %d with %f ms RTT\n", bestTower, 1000 * RTT[bestTower]);
+
+		
+	}
+	
 	i = 0;
 	while(i < 4)
 	{
-		pthread_join(tid[i++], NULL);
+		pthread_cancel(tid[i++]);
+		//pthread_join(tid[i++], NULL);
 		printf("%d:\n", i);
 	}
 
-	/*
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
-	    exit(1);
-	}
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
-
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("client: connect");
-			close(sockfd);
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
-
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-			s, sizeof s);
-	printf("client: connecting to %s\n", s);
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	    perror("recv");
-	    exit(1);
-	}
-
-	buf[numbytes] = '\0';
-
-	printf("client: received '%s'\n",buf);
-
-	close(sockfd);
-	*/
 	return 0;
 }
