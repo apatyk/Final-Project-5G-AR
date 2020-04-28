04b748912fc9a90fe5bd6d7f96f4b5b2d522834f
apatyk <apatyk@tower-0.finalproject-group4.ch-geni-net.instageni.colorado.edu>
2020-04-28 12:19:18 -0600

    Adding delay simulator script

---
+++ sim_delay.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## loop forever, randomly assigning bandwidth
+clear
+while [ : ]
+do
+	## display date
+	tput cup 1 5
+	date
+	## display hostname
+	tput cup 2 5
+	echo "Hostname : $(hostname)"
+	## randomly limit delay between 0ms & 100ms
+	DELAY=$[RANDOM % 100]
+	sudo tc qdisc replace dev eth1 root netem delay ${DELAY}ms 1ms limit 1104B
+	tput cup 3 5
+	sudo tc qdisc show dev eth1
+	## wait 1s
+	sleep 1
+done
